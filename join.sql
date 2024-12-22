58d0610f5f158b0e397de7f91369e75a46d8b0e1
Shani Staretz <shakimi88@gmail.com>
2024-12-22 20:01:59 +0200

    added full join

--- join.sql
+++ join.sql
@@ -1,6 +1,23 @@
--- left join inclusive
+--left join inclusive
 select drivers.*, cars.model as car_model, cars.color as car_color from drivers
 left join cars ON drivers.car_id = cars.car_id;
--- left join exlusive
+-- left join exlusive, manualy
 select drivers.*, cars.model as car_model, cars.color as car_color from drivers
 left join cars ON drivers.car_id = cars.car_id where drivers.car_id is null;
+
+-- right join inclusive
+select drivers.*, cars.model as car_model, cars.color as car_color from drivers
+right join cars ON drivers.car_id = cars.car_id;
+
+-- right outer join= exlusive
+select drivers.*, cars.* from drivers
+right join cars ON drivers.car_id = cars.car_id where drivers.driver_id is null;
+
+-- full JOIN inclusive
+select drivers.*, cars.* from drivers
+full join cars ON drivers.car_id = cars.car_id 
+
+-- full OUTER JOIN exclusive
+select drivers.*, cars.* from drivers
+full join cars ON drivers.car_id = cars.car_id where drivers.driver_id is null or drivers.car_id is null
+
